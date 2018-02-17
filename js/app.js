b7529389620c3f7c22a8c76a39843579ee4cb01b
SteffAhv <tefavv22@gmail.com>
2018-02-17 04:50:10 -0500

    añade project section, contact

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 // loading-interactive-parseo-complete
-$(document).ready(function(){
-	
-
+$(document).ready(function () {
+  $(".scroll").click(function (event) {
+    event.preventDefault();
+    $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
+    $('.navbar-default a').removeClass('selected');
+    $(this).addClass('selected');
+  });
 });
